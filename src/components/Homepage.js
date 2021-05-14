53effd02432b3ca10f9a024f6171173aa68d675e
miglekuba <migle.kubiliute@gmail.com>
2021-05-14 11:34:40 +0100

    Build basic App

---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div class="main">
+        <h1>Welcome to Quiz Master</h1>
+      </div>
+    );
+  }
+}
