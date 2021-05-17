b6bd9f813be60df57b0eb545f8d3ce689c389347
miglekuba <migle.kubiliute@gmail.com>
2021-05-17 18:13:47 +0100

    add quizzes route

--- server/controllers/quizzes.js
+++ server/controllers/quizzes.js
@@ -1,5 +1,17 @@
-export const getQuizzes = (req, res) => {
-    res.send('This works!')
-  }
+const Question = require("../models/Question.js");
 
- 
+const getQuizzes = async (req, res) => {
+  try {
+    const createQuiz = await Question.find();
+
+    res.status(200).json(createQuiz);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const createQuiz = (req, res) => {
+  res.send("quiz creation");
+};
+
+module.exports = { getQuizzes, createQuiz };
--- server/routes/quiz-routes.js
+++ server/routes/quiz-routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
 const router = express.Router();
+const {getQuizzes, createQuiz} = require('../controllers/quizzes')
 
-router.get('/', )
+
+router.get('/', getQuizzes )
+router.post('/', createQuiz)
 
 module.exports = router;
