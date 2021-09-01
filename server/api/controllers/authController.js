d6e04dd1fef623f6ef5efb195bd28e8f7ff21fc3
miglekuba <migle.kubiliute@gmail.com>
2021-09-01 18:29:50 +0100

    Add validation handling

--- server/api/controllers/authController.js
+++ server/api/controllers/authController.js
@@ -1,22 +1,64 @@
 const User = require("../../models/User.js");
+//handle errirs
+const handleErrors = (err) => {
+    console.log(err.message, err.code)
+    let errors = { email: '', password: '' };
+
+    //incorrect email
+
+    if (err.message === 'incorrect email'){
+        errors.email = 'that email is not registered'
+    }
+    //incorrect password
+    if (err.message === 'incorrect password'){
+        errors.password = 'that password is incorrect'
+    }
+
+    //duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'that email is already registered';
+        return errors;
+    }
+
+    //validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message
+        });
+    }
+    return errors;
+}
 
 
 const signup_get = (req, res) => {
-    console.log("signup get blah")
     res.sendStatus(204)
 };
 
 const login_get = (req, res) => {
-    console.log("login get blah")
     res.render('login')
 };
 
-const signup_post = (req, res) => {
-    res.send('new sign up')
-};
+const signup_post = async (req, res) => {
+    const { email, password } = req.body;
+    //create and instance of a user and save to db
+    try {
+        const user = await User.create({ email, password })
+        res.status(201).json(user)
 
-const login_post = (req, res) => {
-    res.send('user login')
+    } catch (err) {
+        const errors = handleErrors(err)
+        res.status(400).json({ errors })
+
+    }
+}
+const login_post = async (req, res) => {
+    const { email, password} = req.body;
+    try {
+        const user = await User.login(email, password)    
+    } catch (err) {
+        const errors = handleErrors(err)
+        res.status(400).json({ errors }) 
+    }
 };
 
 module.exports = { signup_get, login_get, signup_post, login_post }
--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,48 @@
 const mongoose = require("mongoose");
+const {isEmail} = require('validator');
 const uuid = require("uuid");
+const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema;
 
 const User = new Schema(
   {
-    id: { type: String, required: true, default: uuid.v4 },
-    username: { type: String, required: true },
-    password: { type: String, required: true },
+    // id: { type: String, required: true, default: uuid.v4 },
+    email: { 
+      type: String,
+      required: [true, 'Please enter email'],
+      unique: true, 
+      lowercase: true,
+      validate: [isEmail, 'Please enter a valid email']
+    },
+
+    password: { type: String, required: [true, 'Please enter the password'] },
     role: { type: String, enum: ["ADMIN", "READ_ONLY_USER", "LIMITED_USER"] },
   },
   { timestamps: true }
 );
+
+User.post('save', function(doc, next) {
+console.log('new user was created and saved', doc)
+next()
+})
+
+//fire a funciton after doc saved to db
+User.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, salt)
+  next();
+})
+User.statics.login = async function(email,password){
+  const user = await this.findOne({email});
+  if (user){
+      const auth = await bcrypt.compare(password, user.password) //comparing hashed password in the db with the one that user logged in
+      if (auth) {
+          return user
+      }
+      throw Error('incorrect password')
+  }
+  throw Error('incorrect email')
+}
+
 
 module.exports = mongoose.model("User", User, "users");
